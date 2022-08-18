7a15a75971dc3edc0ff75186c89c02eebb8ae2c0
brandon <brandon14ogola@gmail.com>
2022-08-18 09:44:52 +0300

    completed assignment

--- spec/control_flow_spec.rb
+++ spec/control_flow_spec.rb
@@ -88,5 +88,60 @@ describe '#calculator' do
   it 'returns nil for invalid operators' do
     expect(calculator('nope', 4, 2)).to eq(nil)
   end
-  
+
 end
+
+
+def  admin_login(username, password) 
+  if username == "admin" || username == "ADMIN" && password == "12345"
+   return "Access granted"
+  else  
+     return "Access denied"
+  end 
+
+ end 
+
+
+def hows_the_weather(temperature)
+  if (temperature < 40)
+    response = "brisk"
+  elsif  (temperature >= 40 && temperature <= 65) 
+    response = "a little chilly"
+  elsif  (temperature > 85)
+    response = "too dang hot"
+  else  
+    response = "perfect"
+  end
+end
+
+
+def fizzbuzz(num)
+  if (num % 3 === 0 && num % 5 === 0)
+    return "FizzBuzz"
+  elsif (num % 3 === 0)
+    return "Fizz"
+  elsif (num % 5 == 0)
+    return "Buzz"
+  else  
+    return num
+  end  
+
+end
+
+def calculator(operation, num1, num2)
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when  "*"
+    num1 * num2
+  when "/"  
+    num1 / num2
+  else
+    puts "Invalid operation!"
+  end
+
+end  
+
+
